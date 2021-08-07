93d4afc9ad1fa89d74d12e217f0ff2e6ffb45b73
Adam Klus <apklus@gmail.com>
2021-08-07 12:53:14 +0100

    Update etl.py

--- etl.py
+++ etl.py
@@ -2,14 +2,17 @@ import pandas as pd
 import os
 import glob
 
+def process_file(cur, filepath, table):
+
 def main():
-    # Get your current folder and subfolder event data
+    # Get your current folder and subfolder data
     filepath = os.getcwd() + '/data'
     # Create a for loop to create a list of files and collect each filepath
     for root, dirs, files in os.walk(filepath):
         # join the file path and roots with the subdirectories using glob
         file_path_list = glob.glob(os.path.join(root,'*'))
-        print(file_path_list)
-        
+    
+
+
 if __name__ == "__main__":
     main()
