7d54de8c9ec6a56c72c80b033638c842673d2ddf
Adam Klus <apklus@gmail.com>
2021-08-08 23:05:33 +0100

    Update etl.py

--- etl.py
+++ etl.py
@@ -9,23 +9,26 @@ def process_file(filepath):
     time.sleep(3)
 
 def getFiles(folderPath):
-    # create a for loop to create a list of files and collect each filepath
+    # loop through files
     for root, dirs, files in os.walk(folderPath):
-        # join the file path and roots with the subdirectories using glob
+        # join the file path and roots with the subdirectories
         file_path_list = glob.glob(os.path.join(root,'*'))
+        # sort so we process older files first
         file_path_list.sort()
+
         return file_path_list
 
-
 if __name__ == "__main__":
-
-    # Get the data folder
+    # get the data folder
     folderPath = os.getcwd() + '/data'
-
+    # nothing processed yet
     processed_files = []
     while True:
+        #get list of files
         files = getFiles(folderPath)
+        # loop through files
         for file in files:
+            # if not yet processed then process and add to processed list
             if file not in processed_files:
                 process_file(file)
                 processed_files.append(file)
