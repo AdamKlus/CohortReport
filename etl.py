a5e94222899b7609b6a2daa3da3fb8dc6a0ffe91
Adam Klus <apklus@gmail.com>
2021-08-08 22:28:58 +0100

    Update etl.py

--- etl.py
+++ etl.py
@@ -1,18 +1,31 @@
 import pandas as pd
+import time
 import os
 import glob
+import sqlite3
 
-def process_file(cur, filepath, table):
+def process_file(filepath):
+    print(filepath + " added")
+    time.sleep(3)
 
-def main():
-    # Get your current folder and subfolder data
-    filepath = os.getcwd() + '/data'
-    # Create a for loop to create a list of files and collect each filepath
-    for root, dirs, files in os.walk(filepath):
+def getFiles(folderPath):
+    # create a for loop to create a list of files and collect each filepath
+    for root, dirs, files in os.walk(folderPath):
         # join the file path and roots with the subdirectories using glob
         file_path_list = glob.glob(os.path.join(root,'*'))
-    
+        file_path_list.sort()
+        return file_path_list
 
 
 if __name__ == "__main__":
-    main()
+
+    # Get the data folder
+    folderPath = os.getcwd() + '/data'
+
+    processed_files = []
+    while True:
+        files = getFiles(folderPath)
+        for file in files:
+            if file not in processed_files:
+                process_file(file)
+                processed_files.append(file)
